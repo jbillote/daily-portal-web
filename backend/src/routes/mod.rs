95be83d78d0ab323f329f345ccc87c6be69cccd3
jbillote <jbillote@gmail.com>
2025-12-10 16:07:11 -0800

    feat: placeholder for API to update uma fans

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,7 +1,12 @@
 mod uma_fans;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 pub fn create_router() -> Router {
-    Router::new().route("/api/uma/fans", get(uma_fans::uma_fans_list_handler))
+    Router::new()
+        .route("/api/uma/fans", get(uma_fans::uma_fans_list_handler))
+        .route("/api/uma/fans", post(uma_fans::uma_fans_update_handler))
 }
